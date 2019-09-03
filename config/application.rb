1f4f5a3f7a8979f2ab9729102af8c9070b43866b
Amit <amitzmn@gmail.com>
2019-09-02 20:00:40 -0400

    Preparing to install ActiveAdmin but BEFORE, added Middleware for ActiveAdmin - Step-B

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module App1RorReact
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Middleware for ActiveAdmin
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
