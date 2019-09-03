e3f6b25cf7ffb0d57e60f734829f82e2c1e6772b
Amit <amitzmn@gmail.com>
2019-09-02 20:04:10 -0400

    Preparing to install ActiveAdmin but BEFORE, in config/application.rb UNCOMMENTED require 'sprockets/railtie since it is needed' - Step-C

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -34,7 +34,7 @@ module App1RorReact
 
     # Middleware for ActiveAdmin
     config.middleware.use Rack::MethodOverride
-    config.middleware.use ActionDispatch::Flash 
+    config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
   end
