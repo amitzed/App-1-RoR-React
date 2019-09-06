59ac183038837063a86d1506d30ca033f613351f
Amit <amitzmn@gmail.com>
2019-09-06 19:34:11 -0400

    Hooked up Supercars resources and scoped API calls to /api -- so now can visit localhost:3000/api/super_cars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :images
-  resources :super_cars
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '/api' do
+    resources :super_cars
+  end
+
 end
