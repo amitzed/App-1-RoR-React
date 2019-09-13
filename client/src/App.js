dc35dcd78a987e961c813029d5e0b93a2d0f2c9e
Amit <amitzmn@gmail.com>
2019-09-13 17:44:50 -0400

    Tested to make sure Frontend connects to backend -- successful

--- client/src/App.js
+++ client/src/App.js
@@ -38,11 +38,49 @@ class App extends Component {
 
 
   render() {
-    return (
-      <div className="App">
-        <h1>Hi</h1>
-      </div>
-    );
+    let {super_cars, super_car} = this.state
+      return super_cars
+      ? <Container text>
+        <Header as='h2' icon textAlign='center' color='red'>
+          <Icon name='car' circular />
+          <Header.Content>
+            Super Cars
+          </Header.Content>
+        </Header>
+        <Divider hidden section />
+        {super_cars && super_cars.length
+          ? <Button.Group color='red' fluid widths={super_cars.length}>
+            {Object.keys(super_cars).map((key) => {
+              return <Button active={super_car && super_car.id === super_cars[key].id} fluid key={key} onClick={() => this.getSuperCar(super_cars[key].id)}>
+              {super_cars[key].title}
+            </Button>
+            })}
+          </Button.Group>
+          : <Container textAlign='center'>No Super Cars Found.</Container>
+        }
+        <Divider section />
+
+          {super_car &&
+            <Container>
+              <Header as='h2'>{super_car.title}</Header>
+              {super_car.description && <p>{super_car.description}</p>}
+              {super_car.ingredients &&
+                <Segment.Group>
+                  {super_car.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
+                </Segment.Group>
+              }
+              {super_car.steps && <p>{super_car.steps}</p>}
+              {super_car.source && <Button basic size='tiny' color='red' href={super_car.source}>Source</Button>}
+            </Container>
+          }
+        </Container>
+        : <Container text>
+          <Dimmer active inverted>
+            <Loader content='Loading' />
+          </Dimmer>
+
+      </Container>
+
   }
 }
 
