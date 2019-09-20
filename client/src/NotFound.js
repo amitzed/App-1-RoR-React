3055c39b72031ecead8a27c2aeb825b944ee6bfd
Amit <amitzmn@gmail.com>
2019-09-19 20:43:34 -0400

    Made edit to 404 file from class to className

--- client/src/NotFound.js
+++ client/src/NotFound.js
@@ -7,8 +7,8 @@ class NotFound extends Component {
     return (
       <Container text textAlign='center'>
         <div id="notfound">
-      		<div class="notfound">
-      			<div class="notfound-404">
+      		<div className="notfound">
+      			<div className="notfound-404">
       				<h3>Oops! Page not found</h3>
       				<h1><span>4</span><span>0</span><span>4</span></h1>
       			</div>
